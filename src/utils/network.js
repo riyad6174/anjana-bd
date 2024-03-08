21150b393dfae7aa4fa4d2ae9a8eab098b78b80f
riyad6174 <alimranhosen21@gmail.com>
2024-03-08 08:43:58 +0600

    change in file path

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://localhost:5000/api/frontend/';
-export const filePath = 'http://localhost:5000/img/';
+export const baseURL = 'http://localhost:5000/api/frontend';
+export const filePath = 'http://localhost:5000/img';
